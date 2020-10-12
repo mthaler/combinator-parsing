46b8dfcc83278bc4c78a9df8e8c52b8e74bbcf12
Michael Thaler <michael.thaler@dlr.de>
2020-10-12 16:13:07 +0200

    Added Arith class.

---
+++ src/main/scala/com/mthaler/combinatorparsing/arith/Arith.scala
@@ -0,0 +1,10 @@
+package com.mthaler.combinatorparsing.arith
+
+import scala.util.parsing.combinator._
+
+class Arith extends JavaTokenParsers {
+  def expr: Parser[Any] = term~rep("+"~term | "-"~term)
+  def term: Parser[Any] = factor~rep("*"~factor | "/"~factor)
+  def factor: Parser[Any] = floatingPointNumber | "("~expr~")"
+}
+
